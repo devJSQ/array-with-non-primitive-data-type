7792756063cf08850defa33dd51acd29d3bcec26
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-03-16 01:15:25 +0300

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,41 @@
+public class Student{
+    private int age;
+    private int id;
+    private String name;
+
+    public Student(int age, int id, String name){
+        this.age = age;
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge (int age){
+        this.age = age;
+
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void  setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName (String name){
+         this.name = name;
+    }
+
+
+
+
+
+}
